b568b55c3619e2ec0e4a87766f3ab0e1a740baec
fabio.fattori <fabio.fattori@PC7322>
2020-12-16 11:49:40 +0100

    aggiunta la caduta della seconda bottiglia ma bisogna correggere il valore di altezza che supera il valore nel while

--- gravità/gravità/MainWindow.xaml.cs
+++ gravità/gravità/MainWindow.xaml.cs
@@ -43,12 +43,26 @@ namespace gravità
             {
                 this.Dispatcher.BeginInvoke(new Action(() =>//scriviamo cosi perchè il wpf è gestito da thread quindi vanno in conflitto:action è un delegato che risolve il conflitto
                 {
-                    img_vino.Margin = new Thickness(301, altezza, 0, 0);
+                    if (altezza != 90)
+                    {
+                        altezza += r.Next(50, 100);
+                        img_cocacola.Margin = (new Thickness(403, altezza, 0, 0));
+                        altezza += r.Next(50, 100);
+                        img_vino.Margin = new Thickness(301, altezza, 0, 0);
+                    }
+                    else
+                    {
+                        img_vino.Margin = new Thickness(301, altezza, 0, 0);
+                        img_cocacola.Margin = (new Thickness(403, altezza, 0, 0));
+                        altezza += 1;
+                    }
+                       
                     
                     
+
                 }));
                 Thread.Sleep(TimeSpan.FromMilliseconds(r.Next(200, 400)));
-                altezza += r.Next(50, 100);
+                
             }
 
 
